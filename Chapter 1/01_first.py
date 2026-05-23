80829c93e19c16a0df44322795f304ed1e91d7b3
Sameer Patra <sam.dev1611@gmail.com>
2026-05-23 15:51:37 +0530

    Added Chapter 1

---
+++ Chapter 1/01_first.py
@@ -0,0 +1,3 @@
+print("Hello World") # This line prints "Hello World" to the console
+print("Hello Sam") # This line prints "Hello Sam" to the console
+print("Hello John") # This line prints "Hello John" to the console
---
+++ Chapter 1/Pyjokes.py
@@ -0,0 +1,8 @@
+import pyjokes  # Import the pyjokes library for programming jokes
+
+# Get a random programming joke
+joke = pyjokes.get_joke() # Retrieve a random joke from the pyjokes library and store it in the variable 'joke'
+
+# Print the joke to the screen
+print(joke) # Output the joke stored in the variable 'joke' to the console
+
